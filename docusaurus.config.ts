69ba4f52fce1c08d6a0914a735031f521fdda680
benjypng <47936793+benjypng@users.noreply.github.com>
2025-11-30 21:45:43 +0800

    chore: configure github pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,6 @@ import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
   title: "Blogseq",
-  tagline: "Blogging with Logseq",
   favicon: "img/favicon.ico",
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -15,15 +14,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://benjypng.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/blogseq",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "benjypng", // Usually your GitHub org/user name.
+  projectName: "blogseq", // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   onBrokenLinks: "throw",
 
